3e90b3160b555bb09dd194c7c324c25dce4775ac
Pavel Bier <pavel.bier@gmail.com>
2011-05-30 12:48:36 +0200

    nasazeni sass
    Signed-off-by: Pavel Bier <pavel.bier@gmail.com>

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'sass'
 #require 'coffee-script'
 
   
@@ -8,6 +9,12 @@ set :public, File.dirname(__FILE__) + '/static'
 #get '/application.js' do
 #    coffee :application
 #end
+
+get '/css/page.css' do
+  	#header 'Content-Type' => 'text/css; charset=utf-8'
+	content_type 'text/css', :charset => 'utf-8'
+	scss :page
+end
 
 get '/' do
 	erb :index
