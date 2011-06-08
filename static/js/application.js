95126109b6ed99c487ec9f9dba2eeaea00e896eb
Pavel Bier <pavel.bier@gmail.com>
2011-06-09 00:23:52 +0200

    doplneni obsahu

--- static/js/application.js
+++ static/js/application.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   var tc = $('#text-container');
   var homeText = tc.html();
+  var targetDate = new Date(2011, 8-1, 6,11,30,0);
+
+  var reinit = function() {
+    $('.fancybox').fancybox();    
+    $('.fancyboxHtml').fancybox({ type:'iframe', width:'80%', height:'80%' });    
+    $('#countdown').countdown({until: targetDate});
+  };
+
 
   $('menu').bind('click',function(e){
     var target = e.target;
@@ -10,6 +18,7 @@ $(document).ready(function(){
         tc.fadeOut('slow',function(){
           tc.html((domid=='#home') ? homeText : $(domid).html());
           tc.fadeIn('slow');
+          reinit();  
         });
       });
       
@@ -19,5 +28,8 @@ $(document).ready(function(){
   if (window.location.hash && window.location.hash != '#home') {
       tc.html($(window.location.hash).html());
   };
+
+
+  reinit();
 
 });
